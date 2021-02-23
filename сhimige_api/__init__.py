bd889f86c210220d7495f81167f949b3f003f402
Ma-Mush <74294847+Ma-Mush@users.noreply.github.com>
2021-02-23 22:19:21 +0500

    Update __init__.py

--- сhimige_api/__init__.py
+++ сhimige_api/__init__.py
@@ -9,8 +9,8 @@ MaMush (vk.com/maks.mushtriev2, t.me/Error_mak25, github.com/Ma-Mush)
 """
 
 import requests
-import pickle
-import os.path
+from json import loads
+from os.path import isfile
 
 class ChimigeError(Exception):
     pass
@@ -18,7 +18,8 @@ class ChimigeError(Exception):
 class Chimige():
     def __init__(self, session_name: str, email: str, password: str, force_login=False):
         """Объявление self-объекта"""
-        self.session_name = session_name + '.session'
+        self.req_sess = requests.Session()
+        self.session_name = session_name + '.txt'
         self.email = email
         self.password = str(password)
         self.cookie = None
@@ -35,64 +36,50 @@ class Chimige():
                         'Referer': 'https://chimige.ru//messages',
                         'TE': 'Trailers'
                         }
+        if not isfile(self.session_name):
+            force_login = True
         self._login(force_login)
 
     def _login(self, force_login=False):
         """Войти в аккаунт"""
         if force_login:
             data = {"username_email": self.email, "password": self.password}
-            r = requests.post("https://chimige.ru//includes/ajax/core/signin.php",
+            r = self.req_sess.post("https://chimige.ru//includes/ajax/core/signin.php",
                               headers=self.headers, data=data)
             self.cookie = "; ".join([f"{c.name}={c.value}" for c in r.cookies])
+            self._save_session()
         else:
             self.cookie = self._get_session()
         self.headers["Cookie"] = self.cookie
 
-    def _not_exited(self):
-        if self.cookie == "exited":
-            raise ChimigeError("Вы уже вышли!")
-        else:
-            return True
-
     def _save_session(self):
         """Сохраняет сессию"""
-        with open(self.session_name, "wb") as f:
-            pickle.dump(self.cookie, f)
+        with open(self.session_name, "w") as f:
+            f.write(self.cookie)
 
     def _get_session(self):
         """Получить сессию"""
-        with open(self.session_name, "rb") as f:
-            return pickle.load(f)
+        with open(self.session_name, "r") as f:
+            return f.read()
 
     def msg(self, text: str, conservation_id: int or str):
         """Отправить сообщение"""
         data = {"message": text, "conversation_id": conservation_id}
-        requests.post("https://chimige.ru//includes/ajax/chat/post.php", headers=self.headers, data=data)
-        requests.post("https://chimige.ru//includes/ajax/data/reset.php", 
+        self.req_sess.post("https://chimige.ru//includes/ajax/chat/post.php", 
+                      headers=self.headers, data=data)
+        self.req_sess.post("https://chimige.ru//includes/ajax/data/reset.php", 
                       headers=self.headers, data={"reset":"messages"})
 
-# Пока не работает 
+# Пока не работает
+# И не будет
 # а как жить
     # def send_photo(self, path, conservation_id, message=None):
-    #     """Отправка фото"""
-    #     if self._not_exited():
-    #         with open(path,'rb') as fil:
-    #             byte = fil.read()
-    #         data ={"secret": "e48ae022bf62df43ac12bb517badcd09", "file":byte, "type":"photos",
-    #                "handle":"chat", "multiple":"false"}
-    #         r = requests.post("https://chimige.ru//includes/ajax/data/upload.php", headers=self.headers, data=data)
-    #         self.r = r
-    #         path = r.request.body
-            # data = {"photo":path, "conservation_id":conservation_id}
-            # if message != None:
-            #     data["message"] = message
-            # requests.post("https://chimige.ru//includes/ajax/chat/post.php", headers=self.headers, data=data)
+
 
     def check_new_msg(self):
-        from json import loads
         data = {"filter":"", "last_request":0, "last_message":"0", "last_notification":0, 
                 "last_post":25, "get":"newsfeed"}
-        _last_message = requests.post("https://chimige.ru//includes/ajax/data/live.php",
+        _last_message = self.req_sess.post("https://chimige.ru//includes/ajax/data/live.php",
                           data=data, headers=self.headers)
         self._last_message = _last_message
         self._last_message._content = loads(_last_message._content)
@@ -102,29 +89,41 @@ class Chimige():
         return False
 
     def get_new_message(self):
+        class _ret_message():
+            pass
         message = self._last_message.message
-        return_mess = {}
         if len(message) >= 10:
-            return_mess["group_chat"] = True
-            return_mess["conservation_id"] = int(message[1].split('"')[3])
-            return_mess["chat_name"] = message[1].split('"')[7]
+            group_chat = True
+            conservation_id = int(message[1].split('"')[3])
+            chat_name = message[1].split('"')[7]
             for i in range(3, len(message)-1):
                 if message[i][1:5] == "text":
-                    return_mess["text"] = message[5].split('"text">')[1].split("</div> <div")[0].split("""
+                    text = message[5].split('"text">')[1].split("</div> <div")[0].split("""
                                 """)[1].split("""
                         """)[0]
+            ret = _ret_message()
+            ret.group_chat = group_chat
+            ret.conservation_id = conservation_id
+            ret.text = text
+            ret.chat_name = chat_name
+            return ret
         elif len(message) == 7:
-            return_mess["group_chat"] = False
-            return_mess["conservation_id"] = int(message[1].split('"')[3])
-            return_mess["user_id"] = int(message[1].split('"')[5])
-            return_mess["user_name"] = message[1].split('"')[7]
-            return_mess["user_short_name"] = message[1].split('"')[11]
-            return_mess["user_photo"] = message[2].split('"')[3]
-            return_mess["text"] = message[5].split('"text">')[1].split("</div> <div")[0].split("""
-                                        """)[1].split("""
+            group_chat = False
+            conservation_id = int(message[1].split('"')[3])
+            user_id = int(message[1].split('"')[5])
+            user_name = message[1].split('"')[7]
+            user_short_name = message[1].split('"')[11]
+            user_photo = message[2].split('"')[3]
+            text = message[5].split('"text">')[1].split("</div> <div")[0].split("""
+                                            """)[1].split("""
                                 """)[0]
-        else:
-            print(message)
-            print("=====")
-            print(len(message))
-        return return_mess
+            ret = _ret_message()
+            ret.group_chat = group_chat
+            ret.conservation_id = conservation_id
+            ret.text = text
+            ret.user_id = user_id
+            ret.user_name = user_name
+            ret.user_short_name = user_short_name
+            ret.user_photo = user_photo
+            return ret
+        return message
